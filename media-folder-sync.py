ba0ef66ebe0cb54f57c2f6a86247aec4990cb281
Thomaz de Oliveira dos Reis <thomaz.reis@ebc.com.br>
2013-01-24 09:01:05 -0200

    Update to use database to persist filechecks, improved documentation

--- media-folder-sync.py
+++ media-folder-sync.py
@@ -7,6 +7,9 @@ import subprocess
 import tempfile
 from optparse import OptionParser
 from itertools import chain
+import sqlite3
+import UserDict
+from sqlite3 import dbapi2 as sqlite
 
 # List of group of filetypes. Each dictionary in the list is all extensions 
 # and codecs that needs to exist for this kind of file
@@ -17,10 +20,58 @@ filetypes = [
     },
 ]
 
+class dbdict(UserDict.DictMixin):
+    ''' dbdict, a dictionnary-like class that persists in a sqlite file 
+        Thanks to: http://sebsauvage.net/python/snyppets/index.html#dbdict
+    '''
+    def __init__(self,db_filename):
+        self.db_filename = db_filename
+        if not os.path.isfile(self.db_filename):
+            self.con = sqlite.connect(self.db_filename)
+            self.con.execute("create table data (key PRIMARY KEY,value)")
+        else:
+            self.con = sqlite.connect(self.db_filename)
+   
+    def __getitem__(self, key):
+        row = self.con.execute("select value from data where key=?",(key,)).fetchone()
+        if not row: raise KeyError
+        return row[0]
+   
+    def __setitem__(self, key, item):
+        if self.con.execute("select key from data where key=?",(key,)).fetchone():
+            self.con.execute("update data set value=? where key=?",(item,key))
+        else:
+            self.con.execute("insert into data (key,value) values (?,?)",(key, item))
+        self.con.commit()
+              
+    def __delitem__(self, key):
+        if self.con.execute("select key from data where key=?",(key,)).fetchone():
+            self.con.execute("delete from data where key=?",(key,))
+            self.con.commit()
+        else:
+             raise KeyError
+            
+    def keys(self):
+        return [row[0] for row in self.con.execute("select key from data").fetchall()]
+
 # Store m_time of all files to check if the file has been updated by the user
 # or by the system itself
 filechecks = {}
 check = lambda filepath: os.stat(filepath).st_mtime
+
+def remove_related_files(filepath):
+    """ Remove filepath and related files (from the same name and group of
+        extension) from filesystem and filechecks
+    """
+    path, extension = os.path.splitext(filepath)
+    filetype = get_type(extension)
+    for ext in filetype:
+        filepath=path+ext
+        print "deleting:", filepath
+        if filechecks.has_key(filepath):
+            del filechecks[filepath]
+        if os.path.exists(filepath):
+            os.remove(filepath)
 
 def ffmpeg(file_in, file_out, codecs):
     """ Calls ffmeg to convert file_in to file_out using codecs
@@ -35,8 +86,7 @@ def ffmpeg(file_in, file_out, codecs):
             error_output = tmp.read()
             print "Error while converting."
             return error_output
-    filechecks[file_out] = check(file_out)
-    print "Done."
+    print "conversion done."
     
 def get_type(extension):
     """ Return the group of filetypes, defined globally, that extension is
@@ -46,11 +96,9 @@ def get_type(extension):
         if extension in filetype.keys():
             return filetype
 
-def verify(filepath, ignore_check=False):
+def verify(filepath):
     """ Check if all the extensions has been created for filepath extension
         group, and creates if doesn't.
-        Ignore check ignores the check from "filechecks" creating files if
-        doesn't exists.
     """
     path, extension = os.path.splitext(filepath)
     filetype = get_type(extension)
@@ -58,25 +106,38 @@ def verify(filepath, ignore_check=False):
     if not filetype:
         return
     
-    print "File detected:",filepath
-    if not ignore_check and filechecks.get(filepath,None) == check(filepath):
+    filecheck = filechecks.get(filepath,None)
+    
+    if  filecheck == check(filepath):
         return
+    
+    if filecheck:
+        print "File change detected:",filepath
+    else:
+        print "New file detected:",filepath
+    
     filechecks[filepath] = check(filepath)
     
     for ext,codecs in filetype.items():
         new_file = path+ext
+        
         if new_file == filepath:
             continue
         
-        if os.path.exists(new_file) and ignore_check:
+        if filecheck == None and os.path.exists(new_file):
+            print 'A unknown file detected: %s. Assuming it is correct.' %new_file
+            filechecks[new_file] = check(new_file)
             continue
         
         error = ffmpeg(filepath,new_file,codecs)
-        if not error:
+
+        if error:
+            with open(path+'.error','w') as error_file:
+                error_file.write(error)
             continue
         
-        with open(path+'.error','w') as error_file:
-            error_file.write(error)
+        filechecks[new_file] = check(new_file)
+        
     
 class Process(ProcessEvent):
     """ Process class that is connected to WatchManager.
@@ -88,13 +149,29 @@ class Process(ProcessEvent):
         self.mask = mask
         
     def process_IN_CREATE(self, event):
+        """ File or directory has been created.
+            If a folder is created, add it to be monitored
+        """
         path = os.path.join(event.path, event.name)
         if os.path.isdir(path):
             self.wm.add_watch(path, self.mask, rec=True)
 
     def process_IN_CLOSE_WRITE(self, event):
+        """ File has been closed after a write.
+            The verification is done here to make sure
+            that everything was written to the file before
+            any conversion starts
+        """
         filepath = os.path.join(event.path, event.name)
         verify(filepath)
+    
+    def process_IN_DELETE(self, event):
+        """ File or directory has been deleted.
+            If a file is deleted, remove it from filechecks
+        """
+        filepath = os.path.join(event.path, event.name)
+        if filechecks.has_key(filepath):
+            remove_related_files(filepath)
 
 def monitor_loop(folders):
     """ Main loop, create everything needed for the notification
@@ -102,7 +179,7 @@ def monitor_loop(folders):
         happens.
     """
     wm = WatchManager()
-    mask = IN_CLOSE_WRITE | IN_CREATE
+    mask = IN_CLOSE_WRITE | IN_CREATE | IN_DELETE
     process = Process(wm, mask)
     notifier = Notifier(wm, process)
     for folder in folders:
@@ -124,10 +201,17 @@ def get_all_files(folder):
             yield os.path.join(dirpath, filename)
 
 def verify_all(folders):
-    """ Calls verify for all files found on all specified folders
+    """ Calls verify for all files found on all specified folders,
+        and remove all non existant files.
     """
+    # Add/Update all files
     for filepath in chain(*map(get_all_files,folders)):
-        verify(filepath, ignore_check=True)
+        verify(filepath)
+    
+    #Remove non existant files
+    for filepath in filechecks:
+        if not os.path.exists(filepath):
+            remove_related_files(filepath)
 
 def command_line_args():
     """ Deals with command line arguments
@@ -138,18 +222,28 @@ def command_line_args():
         action="store_false", help="Does not verify all folders contents before start monitor")
     parser.add_option("-m", "--no-monitor", dest="monitor", default=True,
         action="store_false", help="Does not start folder monitor")
+    parser.add_option("-d", "--database", dest="database", default=os.path.expanduser('~/.media-folder-sync.db'),
+        help="Change SQLite database file")
+        
     return parser.parse_args()
 
 def start():
     """ Starts application
     """
+    global filechecks
     options, folders = command_line_args()
+    filechecks = dbdict(options.database)
+    
     if not folders:
         folders.append(os.getcwd())
+    
+    folders = [os.path.abspath(folder) for folder in folders]
+    
     if options.verify:
         print "Verifying existing files..."
         verify_all(folders)
         print "All files verified."
+        
     if options.monitor:
         monitor_loop(folders)
 
