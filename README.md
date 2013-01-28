ed76a8e0b66d70f1991ae45545ba44a19031c02a
Thomaz de Oliveira dos Reis <thomaz.reis@ebc.com.br>
2013-01-28 09:36:24 -0200

    Update readme, change settings to last import so it can replace imported stuff if user wants

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ media_folder_sync
 For improved html5 support, most servers needs to keep 2 or more copys of your media file in different formats. This script monitors a specific folder, using Linux Inotify for changes, and uses ffmpeg to convert your video/audio for all formats wanted.
 <br/>
 This version here keeps an ogg and mp3 version for all submited audios (in ogg or mp3 format).
-To modify or improve this, edit media_folder_sync.py and update the filetypes list.
+To modify or improve this, edit settings.py and update the filetypes list.
 Each dictionay inside filetypes list mean a group of media elements with it's ffmpeg codec.
 Lets say that you need to have, for audio, formats mp3 and ogg and for video webm and mp4. You will do something like that:
 
--- media_folder_sync.py
+++ media_folder_sync.py
@@ -7,55 +7,8 @@ import subprocess
 import tempfile
 from optparse import OptionParser
 from itertools import chain
-import sqlite3
-import UserDict
-from sqlite3 import dbapi2 as sqlite
-
-# List of group of filetypes. Each dictionary in the list is all extensions 
-# and codecs that needs to exist for this kind of file
-filetypes = [
-    {
-        '.ogg': {'acodec':'libvorbis'},
-        '.mp3': {'acodec':'libmp3lame'},
-    },
-]
-
-# FFmpeg executable name or path
-ffmpeg_exec = 'ffmpeg'
-
-class dbdict(UserDict.DictMixin):
-    ''' dbdict, a dictionnary-like class that persists in a sqlite file 
-        Thanks to: http://sebsauvage.net/python/snyppets/index.html#dbdict
-    '''
-    def __init__(self,db_filename):
-        self.db_filename = db_filename
-        if not os.path.isfile(self.db_filename):
-            self.con = sqlite.connect(self.db_filename)
-            self.con.execute("create table data (key PRIMARY KEY,value)")
-        else:
-            self.con = sqlite.connect(self.db_filename)
-   
-    def __getitem__(self, key):
-        row = self.con.execute("select value from data where key=?",(key,)).fetchone()
-        if not row: raise KeyError
-        return row[0]
-   
-    def __setitem__(self, key, item):
-        if self.con.execute("select key from data where key=?",(key,)).fetchone():
-            self.con.execute("update data set value=? where key=?",(item,key))
-        else:
-            self.con.execute("insert into data (key,value) values (?,?)",(key, item))
-        self.con.commit()
-              
-    def __delitem__(self, key):
-        if self.con.execute("select key from data where key=?",(key,)).fetchone():
-            self.con.execute("delete from data where key=?",(key,))
-            self.con.commit()
-        else:
-             raise KeyError
-            
-    def keys(self):
-        return [row[0] for row in self.con.execute("select key from data").fetchall()]
+from dbdict import dbdict
+from settings import *
 
 # Store m_time of all files to check if the file has been updated by the user
 # or by the system itself
